27e37b24935a6517a995456b38d89bb1bfbebd74
maria <exxxxxxe90@gmail.com>
2026-03-04 22:35:54 +0300

    Create GlobusWPF

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,20 @@
-import GlassCard from "@/components/GlassCard";
 import LandingPage from "@/components/LandingPage";
 import LoginPage from "@/components/LoginPage";
 
 export default function AuthSplitPage() {
     return (
         // Контейнер на весь экран, разделенный на две колонки
-        <main className="min-h-full flex flex-col md:flex-row">
+        <main className="w-screen flex flex-col md:flex-row">
             {/* Левая часть: Landing (занимает 50% на больших экранах) */}
-            <section className="w-full md:w-1/2 flex justify-center border-b md:border-b-0 md:border-r border-white/10">
+            <section className="md:w-2/3 flex border-b md:border-b-0 md:border-r border-primary/10">
                 <LandingPage />
             </section>
 
             {/* Правая часть: Login (занимает 50% на больших экранах) */}
-            <section className="w-screen h-screen md:w-1/2 flex justify-center">
-                <GlassCard
-                    className="w-full flex justify-center p-8 md:p-12"
-                    intensity="high"
-                >
+            <section className="h-screen md:w-1/3 flex justify-center">
+                <div className="flex justify-center p-8 md:p-12">
                     <LoginPage />
-                </GlassCard>
+                </div>
             </section>
         </main>
     );
